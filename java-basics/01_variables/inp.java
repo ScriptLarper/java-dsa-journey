f3a0ef1001d64a11fdfda8f4afd1d513a26d5210
ScriptLarper <somyadev77@gmail.com>
2026-06-08 00:33:37 +0530

    data types

--- java-basics/01_variables/inp.java
+++ java-basics/01_variables/inp.java
@@ -1,10 +1,15 @@
 import java.util.Scanner;
+
 public class inp{
     public static void main(String[] args){
+        System.out.println("what is your name ");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name: ");
         String name = sc.nextLine();
-        System.out.println("Hello, " + name + "!");         
 
+        System.out.println("what is your last name ");
+        Scanner sd = new Scanner(System.in);
+        String lastname = sc.nextLine();
+
+        System.out.println(name+ "" +lastname);
     }
 }
---
+++ java-basics/01_variables/primitive.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class primitive{
+    public static void main(String[] args){
+        int num = 4;
+        char him = 'h';
+        boolean s = true;
+        float m = 4.545453f;
+        double sm = 234455677;
+        long ed = 2344243423425L;
+
+        System.out.println("Enter your age");
+        Scanner rad = new Scanner(System.in);
+        int ans = rad.nextInt();
+        System.out.println("Your age is " + ans);
+
+        //input of char
+
+        System.out.println("Enter fav letter");
+        Scanner letter = new Scanner(System.in);
+        
+
+
+
+    }
+}
