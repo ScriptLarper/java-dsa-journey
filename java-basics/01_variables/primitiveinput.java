310d8a9495438dc5f6fd6727b66afae8fecf4258
ScriptLarper <somyadev77@gmail.com>
2026-06-10 22:51:37 +0530

    sum of 2 numbers

---
+++ java-basics/01_variables/primitiveinput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class primitiveinput {
+    public static void main(String[] args){
+        //int
+        System.out.println("Enter your 1st numbers");
+        Scanner inp = new Scanner(System.in);
+        int first = inp.nextInt();
+
+        System.out.println("Enter your 2nd numbers");
+        Scanner inpm = new Scanner(System.in);
+        int sec = inp.nextInt();
+
+        int add = first+sec;
+        System.out.println("The answer is = " + add);
+
+        //
+
+
+
+
+    }
+    
+}
